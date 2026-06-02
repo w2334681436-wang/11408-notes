836482868ab7238e7c7283f124bcca37d96ce9d7
w2334681436-wang <w2334681436@gmail.com>
2026-06-02 11:05:09 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '20260602-1430-pagesearch';
+const APP_VERSION = '20260602-1515-pdfexport';
 const SW_VERSION = new URL(self.location.href).searchParams.get('v') || APP_VERSION;
 const CACHE_NAME = `11408-notes-cache-${SW_VERSION}`;
 
